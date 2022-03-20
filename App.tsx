37d8edaeed5a081d0091ab595163150a61fe0311
budimanfajarf <budimanfajarf@gmail.com>
2022-03-20 17:39:53 +0700

    Write more like "expo init" but with my personal preferences

--- App.tsx
+++ App.tsx
@@ -1,21 +1,17 @@
 import { StyleSheet, Text, View } from 'react-native';
 
-const App = () => {
-  return (
-    <View
-      style={styles.container}
-    >
-      <Text>Hello World!</Text>
-    </View>
-  )
-}
-
-export default App;
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-})
+});
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World!</Text>
+    </View>
+  );
+}
