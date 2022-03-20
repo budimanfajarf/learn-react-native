e865403fd3beb6fcbb8c826de2377749d45998dd
budimanfajarf <budimanfajarf@gmail.com>
2022-03-20 16:51:29 +0700

    Move the style of the main view to StyleSheet const in "container" property

--- App.tsx
+++ App.tsx
@@ -1,13 +1,9 @@
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 const App = () => {
-  return(
+  return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
       <Text>Hello World!</Text>
     </View>
@@ -15,3 +11,11 @@ const App = () => {
 }
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+})
